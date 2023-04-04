694f754e84edb8b9e412415710dabc895bf4d171
mdowns <michaeljdowns99@gmail.com>
2023-04-04 09:16:00 -0600

    Added Style Stage

--- js/displayProjects.js
+++ js/displayProjects.js
@@ -58,8 +58,9 @@ function projectSelection()
     <option value= 4 >5 Day Weather API</option> 
     <option value= 5 >BYUI Home Page Rebuild</option>
     <option value= 6 >C# ATM</option>
-    <option value= 7 >Data Structures Presentation</option>
-    <option value= 8 >Commerce Website</option>  
+    <option value= 7 >Style Stage</option>
+    <option value= 8 >Data Structures Presentation</option>
+    <option value= 9 >Commerce Website</option>  
     
     </select>`;
 
